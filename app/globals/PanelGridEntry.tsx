519eb1f279f24501496421db05dcbf2b84a24ced
Jesper <jthabo+oemf@gmail.com>
2026-03-19 22:40:54 +0100

    added no-cache force-dynamic

--- app/globals/PanelGridEntry.tsx
+++ app/globals/PanelGridEntry.tsx
@@ -4,6 +4,8 @@ import prisma from "@/lib/prisma";
 import EditFrivilligForm from "./EditFrivilligForm";
 import AddShift from "./AddShift";
 
+export const dynamic = "force-dynamic";
+
 export default async function PanelGridEntry() {
   const frivillige = await prisma.frivillig.findMany({
     include: {
